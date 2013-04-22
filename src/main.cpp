d2a6614f3604b3cba7b3f1adbf4ff21a7ba602ed
Mario S. Könz <mskoenz@gmx.net>
2013-04-22 19:37:43 -0400

    2.Commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+// Author:  Mario S. Könz <mskoenz@gmx.net>
+// Date:    22.04.2013 19:37:14 EDT
+// File:    main.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
